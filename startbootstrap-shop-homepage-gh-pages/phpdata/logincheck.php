66bd26413c2d3fbd72c8a47f1b1010a9362a4dcb
carlacodes <carla.griffiths@gmail.com>
2019-02-09 14:21:21 +0000

    221PM 09_02_2019

---
+++ startbootstrap-shop-homepage-gh-pages/phpdata/logincheck.php
@@ -0,0 +1,43 @@
+<?php
+
+//making this a function
+
+require_once('databasephp.php');
+
+//ALTER THE TABLE DEFAULT VALUES FOR ALL THE REQUIRED COLUMNS 05/02/2019
+mysqli_query($link,"ALTER TABLE customers
+/*ALTER COLUMN store_id SET DEFAULT NULL,
+ALTER COLUMN address_id SET DEFAULT NULL,
+ALTER COLUMN active SET DEFAULT '1',
+ALTER COLUMN create_date SET DEFAULT CURRENT_TIMESTAMP*/
+))");
+
+//$firstName = $_POST['first_name'] ?? '1'; //dataphp 7.0
+//$familyName = $_POST['last_name'] ?? '1';
+$emailSql=$_POST['email']; //want to retrieve email
+$passwordSQL=$_POST['password'];
+/*$qryAdd = "INSERT INTO customer (first_name, last_name, email) VALUES (";
+$qryAdd .= "'" . $firstName . "', '" . $familyName . "', '" . $emailSql . "')";*/
+
+$qryFind = "SELECT * FROM customers ";
+$qryFind .= " WHERE password = '" . $passwordSQL ."' AND email = '" . $emailSql . "'";
+
+$connection = connectToDb();
+
+//Check if the name exists
+$result = mysqli_query($connection, $qryFind);
+if (empty($result)){
+    echo "The result is empty";
+}
+if (mysqli_num_rows($result) > 0) {
+    echo "You are Logged In";
+    $user = mysqli_fetch_assoc($result);
+    echo "Welcome";
+    echo $user['first_name'];
+    echo $user['last_name'];
+    $GLOBALS['userID']=$user['customerID'];
+} else {
+        echo "Cannot find the selected user. Please try again or create a new account.";
+        echo "$emailSql, $passwordSQL";
+        closeDb($connection);
+}
